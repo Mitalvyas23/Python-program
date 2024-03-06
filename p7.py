e053d1c7cb5f62822117a6870320b8bb52231a3f
Mital vyas <159806176+Mitalvyas23@users.noreply.github.com>
2024-03-06 01:31:59 -0800

    Python program
    program 7

---
+++ p7.py
@@ -0,0 +1,16 @@
+def length(list):
+    leng = int(input('enter list length : '))
+    print('enter elements in list : ')
+    for i in range(0,leng):
+        n = int(input('list[] : '))
+        list.append(n)
+
+    print('your list is : ')
+    print(list)
+    count = 0
+    for i in list:
+        count = count+1
+
+    print('LIST LENGTH IS : ',count)
+
+length([])
