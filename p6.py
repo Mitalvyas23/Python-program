fb3c63955e19560b577dd6ccb01ebe883f795ccc
Mital vyas <159806176+Mitalvyas23@users.noreply.github.com>
2024-03-06 01:30:59 -0800

    Python program
    Program 6

---
+++ p6.py
@@ -0,0 +1,23 @@
+def swap(list):
+    n1 = int(input('choose one element in a list : '))
+    n2 = int(input('choose second element in list : '))
+    print('your elements are : ')
+    print(n1,n2)
+    print('your list before swap : ')
+    print(list)
+
+    for i in range(0,len(list)):
+        if(n1==list[i]):
+            j = i
+    
+    for i in range(0,len(list)):
+        if(n2==list[i]):
+            k = i
+
+    temp = list[j]
+    list[j] = list[k]
+    list[k] = temp
+    print('your list after swap : ')
+    print(list)
+
+swap([11,22,33,44,55,66,77,88,99])
